5304a79e0bac7d415c338914b42f8f7a82ee869c
dbromle2 <36002560+dbromle2@users.noreply.github.com>
2020-10-30 23:06:14 -0400

    both subject and component now parse from the json

--- static/lab3.js
+++ static/lab3.js
@@ -21,22 +21,33 @@ request.onload = function(){
 //Initiation function to load/populate the web page correctly
 function populateOptions(jsonObj){
     var subjectSelect = document.getElementById("subjectInput");
+    var componentSelect = document.getElementById("componentInput");
     const myCourse = jsonObj;
 
     let subjectArr = [];
-
+    let componentArr = [];
     for (var i=0; i<myCourse.length; i++){
+        //Populate the Subject Select fields
         if (subjectArr.includes(myCourse[i].subject)){
-
+            //yes it's jank but it works shut up
         } else {
             let subjectOption = document.createElement("option");
-            //const componentOption = document.createElement("option");
 
             subjectArr[i] = myCourse[i].subject;
             subjectOption.text = myCourse[i].subject;
-            //componentOption = parsedJSON[i].ssr_component;
 
             subjectSelect.add(subjectOption);
+        }
+        //Populate the Component Select fields
+        if (componentArr.includes(myCourse[i].course_info[0].ssr_component)){
+            //yes it's jank but it works shut up
+        } else {
+            let componentOption = document.createElement("option");
+
+            componentArr[i] = myCourse[i].course_info[0].ssr_component;
+            componentOption.text = myCourse[i].course_info[0].ssr_component;
+
+            componentSelect.add(componentOption);
         }
     }
 }
