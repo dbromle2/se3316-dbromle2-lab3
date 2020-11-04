098147ea895440b2d0af728ddbbc4f4d81f3d0fd
dbromle2 <36002560+dbromle2@users.noreply.github.com>
2020-11-03 21:37:05 -0500

    Front end Step 1 complete

--- static/lab3.js
+++ static/lab3.js
@@ -13,36 +13,68 @@
 //     populateOptions(myResponse);
 // }
 
-//Initiation function to load/populate the web page correctly
-function populateOptions(jsonObj){
-    var subjectSelect = document.getElementById("subjectInput");
-    var componentSelect = document.getElementById("componentInput");
-    const myCourse = jsonObj;
+function onSearch(){
+    let subSelIn = document.getElementById("subjectInput");
+    let corSelIn = document.getElementById("catalogInput");
+    let comSelIn = document.getElementById("componentInput");
+    let subSel = subSelIn.value;
+    let corSel = corSelIn.value;
+    let comSel = comSelIn.value;
 
-    let subjectArr = [];
-    let componentArr = [];
-    for (var i=0; i<myCourse.length; i++){
-        //Populate the Subject Select fields
-        if (subjectArr.includes(myCourse[i].subject)){
-            //yes it's jank but it works shut up
-        } else {
-            let subjectOption = document.createElement("option");
+    console.log("Server sent these inputs:  " + subSel + " " + corSel + " " + comSel + " "); //testing
 
-            subjectArr[i] = myCourse[i].subject;
-            subjectOption.text = myCourse[i].subject;
+    let request = new XMLHttpRequest();
 
-            subjectSelect.add(subjectOption);
-        }
-        //Populate the Component Select fields
-        if (componentArr.includes(myCourse[i].course_info[0].ssr_component)){
-            //yes it's jank but it works shut up
-        } else {
-            let componentOption = document.createElement("option");
 
-            componentArr[i] = myCourse[i].course_info[0].ssr_component;
-            componentOption.text = myCourse[i].course_info[0].ssr_component;
+    if(subSel == "All Subjects" && corSel == "" && comSel == "All"){
+        //Default entries, Step 1 Get all subjects and descriptions
+        request.open("GET", "/courses", true);
+        request.responseType = "json";
+        request.send();
+    } else if(subSel != "All Subjects" && corSel == ""){
+        //Only subject code specified, Step 2 Get all course codes for a given subject code
+        request.open("GET", "/courses/"+subSel, true);
+        request.responseType = "json";
+        request.send();
+    }
 
-            componentSelect.add(componentOption);
-        }
+    request.onload = function(){
+        const myResponse = request.response;
+        populateOuts(myResponse);
     }
 }
+
+//Function to populate the outputs
+function populateOuts(jsonObj){
+    let outList = document.getElementById("outList");
+    let flavourText = document.getElementById("flavourText");
+    const myCourse = jsonObj;
+
+    //refresh the html every time a new response comes from the server
+    while(outList.hasChildNodes()){
+        outList.removeChild(outList.firstChild);
+    }
+
+    flavourText.innerHTML = ("Search results: ");
+    for (var i=0; i<myCourse.length; i++){
+        //Populate the Subject Select fields
+        let courseLi = document.createElement("li");
+        
+        courseLi.innerHTML = myCourse[i];
+
+        outList.appendChild(courseLi);
+
+        // //Populate the Component Select fields
+        // if (componentArr.includes(myCourse[i].course_info[0].ssr_component)){
+        //     //yes it's jank but it works shut up
+        // } else {
+        //     let componentOption = document.createElement("option");
+
+        //     componentArr[i] = myCourse[i].course_info[0].ssr_component;
+        //     componentOption.text = myCourse[i].course_info[0].ssr_component;
+
+        //     componentSelect.add(componentOption);
+        // }
+    }
+}
+
